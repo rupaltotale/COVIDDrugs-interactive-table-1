3d3442c392184584e916bd8bb77fa6915b97ef93
Ashok Parasa <ashingtondc@gmail.com>
2020-04-29 23:58:42 -0400

    initial

---
+++ main.js
@@ -0,0 +1,36 @@
+let xhr = new XMLHttpRequest();
+let url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRB8GpRwi3-e3VjPx8z3IXRRf6O6Z2lfhIWF9OfmH1VeD-QAALEJOlCLrreqCYmqxwfn3wWpBrZNM8l/pub?gid=0&single=true&output=csv";
+xhr.open("GET", url);
+xhr.responseType = "text";
+
+xhr.onload = function () {
+    convert(this.responseText);
+};
+
+xhr.send();
+
+function convert(data)
+{
+    let json = Papa.parse(data, {header: true});
+    console.log(json.data);
+    display(json.data);
+}
+
+function display(json)
+{
+    //create Tabulator on DOM element with id "table"
+    let table = new Tabulator("#table", {
+        height:720,
+        data:json, //assign data to table
+        autoColumns:true,
+    });
+
+    let columns = table.getColumnDefinitions();
+
+    // basic code to adapt to your need
+    columns.forEach(column => {
+        column.headerFilter = true;
+    });
+
+    table.setColumns(columns);
+}
